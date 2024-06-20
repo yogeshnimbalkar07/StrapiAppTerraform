3edeb6ef7747848357bc524348f191fa086d8d10
yogeshnimbalkar07 <162571678+yogeshnimbalkar07@users.noreply.github.com>
2024-06-20 19:52:11 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "strapi1" {
+resource "aws_instance" "strapi" {
   ami           = "ami-0f58b397bc5c1f2e8" 
   instance_type = var.instance_type
   key_name      = var.key_name
